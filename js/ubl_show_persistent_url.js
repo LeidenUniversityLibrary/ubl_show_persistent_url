a0293dbabbbf61bdacc4a7d72f4937b4e1c0e266
lucasvanschaik <schaik@library.leidenuniv.nl>
2017-04-20 03:42:28 -0400

    Version 2 Add button to the detail tools with possibility to copy the persistent url by the user.

--- js/ubl_show_persistent_url.js
+++ js/ubl_show_persistent_url.js
@@ -4,17 +4,79 @@
  */
 
 jQuery(document).ready(function() {
-  var pagetitle = jQuery('#page-title');
-  var button = jQuery(pagetitle).find('.ublpersistentbutton');
-  if (pagetitle && button) {
-    var title = button.data('title');
-    if (title) {
-      button.html(title);
-    }
-    button.click(function (e) {
-      var url = jQuery(this).attr('href');
-      prompt('Copy / paste the URL below',url);
+  jQuery('.ubl-detail-tools LI A .fa-link').parent().click(function (e) {
+    var $li = jQuery(this).parent();
+    var $additionalblock = jQuery('DIV.link-additional');
+    if ($additionalblock.size() > 0) {
       e.preventDefault();
-    }); 
-  }
+      if ($additionalblock.is(':visible')) {
+        return; // is already visible
+      }
+
+      var x = $li.offset().left - $additionalblock.outerWidth() + $li.outerWidth();
+      if (x > 0) {
+        $additionalblock.css('left', x + 'px');
+      }
+      var y = $li.offset().top + $li.outerHeight();
+      $additionalblock.css('top', y + 'px');
+
+      $additionalblock.find('#persistenturl').on('click change keyup keydown keypress', function(e) {
+        this.select();
+        if ((e.ctrlKey || e.metaKey) && (e.keyCode == 67)) {
+          // user is copying
+          $additionalblock.find('.howtocopy').html('The URL is copied!');
+        }
+        else {
+          e.stopPropagation(); 
+          e.preventDefault();
+        }
+      });
+
+      if ($additionalblock.find('.howtocopy').size() == 0) {
+        var $howtocopy = jQuery('<DIV class="howtocopy"/>');
+        $additionalblock.find('.menu').append($howtocopy);
+        var copyfunc = function() {
+          if (navigator.platform.indexOf('Mac') == 0) {
+            $howtocopy.html('Press Cmd-C to copy');
+          }
+          else {
+            $howtocopy.html('Press Ctrl-C to copy');
+          }
+        };
+        if (document.execCommand && document.queryCommandSupported && document.queryCommandSupported('copy')) {
+          var $button = jQuery('<BUTTON type="button">Copy URL</BUTTON>'); 
+          $howtocopy.append($button);
+          $button.click(function(e) {
+            e.preventDefault();
+            
+            $additionalblock.find('#persistenturl').select();
+            var copied = false;
+            try {
+              copied = document.execCommand('copy');
+            }
+            catch (err) {
+              console.log('Oops, unable to copy:' + err);
+              copied = false;
+            }
+            if (!copied) {
+              e.stopPropagation();
+              copyfunc();
+            } 
+          });
+        }
+        else {
+          copyfunc();
+        }
+      }
+
+      $additionalblock.slideDown(function() {
+        $additionalblock.find('#persistenturl').select();
+      });
+
+      e.stopPropagation();
+      jQuery('BODY').one('click', function() {
+        $additionalblock.slideUp();
+      });
+    }
+  });
 })
